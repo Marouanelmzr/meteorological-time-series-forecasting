5174655c271104f2bf710f6f9ea49894cc7e5bee
Marouanelmzr <elmozariahimarouane05@gmail.com>
2026-07-08 15:21:01 +0100

    First visualusation function

---
+++ src/visualization/target_distribution.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def plot_target_distribution(df):
+    counts = df["has_gust"].value_counts().sort_index()
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.bar(["No Gust", "Gust"], counts.values )
+    ax.set_title("Target Distribution")
+    ax.set_ylabel("Number of samples")
+
+    plt.tight_layout()
+
+    return fig
