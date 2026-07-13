028bf6ef2024e4229178da17a2e75f637806c50f
Marouanelmzr <elmozariahimarouane05@gmail.com>
2026-07-13 13:42:50 +0100

    Add METAR 2021-2025 datasett

--- scripts/metar_request.py
+++ scripts/metar_request.py
@@ -1,53 +1,52 @@
 from pathlib import Path
 import pandas as pd
 import requests
-from io import StringIO
 
 ROOT = Path(__file__).resolve().parents[1]
 
 STATIONS_FILE = ROOT / "data" / "cleaned" / "isd_history_station_check.csv"
 OUTPUT_FILE = ROOT / "data" / "raw" / "METAR_2021_2025.csv"
 
+# Load validated stations
 stations = pd.read_csv(STATIONS_FILE)
+
+# Keep only stations found in NOAA
 icaos = stations.loc[stations["found"], "icao"].dropna().tolist()
 
 BASE_URL = "https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py"
 
-all_data = []
+params = [
+    ("data", "all"),
+    ("tz", "UTC"),
+    ("format", "onlycomma"),
+    ("latlon", "yes"),
+    ("elev", "yes"),
+    ("missing", "M"),
+    ("trace", "T"),
+    ("report_type", "1"),
+    ("report_type", "2"),
 
+    ("year1", 2021),
+    ("month1", 1),
+    ("day1", 1),
+
+    ("year2", 2025),
+    ("month2", 12),
+    ("day2", 31),
+]
+
+# Add every ICAO station
 for icao in icaos:
-    print(f"Downloading {icao}...")
+    params.append(("station", icao))
 
-    params = [
-        ("station", icao),
-        ("data", "all"),
-        ("tz", "UTC"),
-        ("format", "onlycomma"),
-        ("latlon", "yes"),
-        ("elev", "yes"),
-        ("missing", "M"),
-        ("trace", "T"),
-        ("report_type", "1"),
-        ("report_type", "2"),
-        ("year1", 2021),
-        ("month1", 1),
-        ("day1", 1),
-        ("year2", 2025),
-        ("month2", 12),
-        ("day2", 31),
-    ]
+print(f"Downloading METAR data for {len(icaos)} stations...")
 
-    r = requests.get(BASE_URL, params=params, timeout=300)
-    r.raise_for_status()
-
-    df = pd.read_csv(StringIO(r.text))
-    all_data.append(df)
-
-metar = pd.concat(all_data, ignore_index=True)
+response = requests.get(BASE_URL, params=params, timeout=300)
+response.raise_for_status()
 
 OUTPUT_FILE.parent.mkdir(parents=True, exist_ok=True)
-metar.to_csv(OUTPUT_FILE, index=False)
 
-print(f"\nDownloaded {len(icaos)} stations.")
-print(f"Total observations: {len(metar):,}")
+with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+    f.write(response.text)
+
 print(f"Saved to:\n{OUTPUT_FILE}")
